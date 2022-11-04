4313170ba97701fbcc998e3e8fd0c0deca7f5ec5
raymondlowe <49975086+raymondlowe@users.noreply.github.com>
2022-11-04 09:43:17 +0800

    Consider list files as csv for easy opening

--- curl-speed-test.cmd
+++ curl-speed-test.cmd
@@ -1,6 +1,6 @@
 for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
 set NOW=%Year%-%Month%-%Day%-%Hour%-%Minute%-%Second%
 
-echo url_effective,http_code, http_connect, time_total, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, size_download, size_upload, size_header, size_request, speed_download, speed_upload, content_type, num_connects, num_redirects > "output-%NOW%.txt"
-for /F "tokens=*" %%A in (urls.txt) do curl -L --output /dev/nul -s -w "@curl-format-line.txt"  %%A >> "output-%NOW%.txt"
+echo url_effective,http_code, http_connect, time_total, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, size_download, size_upload, size_header, size_request, speed_download, speed_upload, content_type, num_connects, num_redirects > "output-%NOW%.csv"
+for /F "tokens=*" %%A in (urls.dsv) do curl -L --output /dev/nul -s -w "@curl-format-line.txt"  %%A >> "output-%NOW%.csv"
 
--- curl-speed-test.sh
+++ curl-speed-test.sh
@@ -1,10 +1,10 @@
 
 now=$(date +"%Y-%m-%d-%H-%M-%S")
-echo url_effective,http_code, http_connect, time_total, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, size_download, size_upload, size_header, size_request, speed_download, speed_upload, content_type, num_connects, num_redirects > "output-${now}.txt"
+echo url_effective,http_code, http_connect, time_total, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, size_download, size_upload, size_header, size_request, speed_download, speed_upload, content_type, num_connects, num_redirects > "output-${now}.csv"
 
 while read url; do
-    curl -L --output /dev/nul -s -w "@curl-format-line.txt"  $url >>  "output-${now}.txt"
-done <urls.txt
+    curl -L --output /dev/nul -s -w "@curl-format-line.txt"  $url >>  "output-${now}.csv"
+done <urls.csv
 
 
 
