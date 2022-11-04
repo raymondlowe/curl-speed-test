19aeab4385ed99f4cd7b58ba10e80ceaa63fa4dd
raymondlowe <49975086+raymondlowe@users.noreply.github.com>
2022-11-04 09:42:19 +0800

    working bash

---
+++ curl-speed-test.sh
@@ -0,0 +1,10 @@
+
+now=$(date +"%Y-%m-%d-%H-%M-%S")
+echo url_effective,http_code, http_connect, time_total, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, size_download, size_upload, size_header, size_request, speed_download, speed_upload, content_type, num_connects, num_redirects > "output-${now}.txt"
+
+while read url; do
+    curl -L --output /dev/nul -s -w "@curl-format-line.txt"  $url >>  "output-${now}.txt"
+done <urls.txt
+
+
+
